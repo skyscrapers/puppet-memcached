b39590706c3085aa1a86123316719efa16b0b323
iuri aranda <iuri.aranda@gmail.com>
2016-08-09 16:55:48 +0200

    Created module to install AWS memcached plugin (#3)
    * Created module to install AWS memcached plugin
    
    * Install and configure the pecl package manually
    the pecl package provider doesn't support installing packages from a local file, also it will be compatible with the oldphp module
    
    * Fix file resource (typo)
    
    * Make aws_php_plugin module compatible with oldphp and php modules
    Now the only thing to sort out is the phpquery command, it comes with the php-common package, but the oldphp module doesn't install it
    
    * Install memcached php extension in a different way for oldphp
    
    * use php5enmod with oldphp module
    
    * Restart php-fpm service after installing memcached extension
    
    * fix tar command
    
    * Uninstall old php memcached extension
    
    * Fix php version parameter checking in aws memcached module
    
    * AWS memcached php plugin has a custom install method for php 7
    
    * There are different version numbers for the AWS memcached plugin
    
    * Parametrized php extension .so file name
    
    * The aws memcached php extension for php 5.6 is compiled with the wrong PHP api version, so droping support for php 5.6 for now
    This is the error PHP throws when trying to launch the extension:
    ```
    PHP Warning:  PHP Startup: memcached: Unable to initialize module
    Module compiled with module API=20121212
    PHP    compiled with module API=20131226
    These options need to match
    ```

---
+++ manifests/aws_php_plugin.pp
@@ -0,0 +1,143 @@
+##### LICENSE
+
+# Copyright (c) Skyscrapers (iLibris bvba) 2016 - http://skyscrape.rs
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class memcached::aws_php_plugin (
+  $php_version = undef
+) {
+
+  validate_string($php_version)
+
+  # Take out the dots of the version number to match the package name
+  $real_php_version = regsubst($php_version, '\.', '', 'G')
+
+  $version = $real_php_version ? {
+    '56' => '1.0.0',
+    '70' => '1.0.0',
+    default => '1.0.1'
+  }
+
+  # Version of the .so file
+  $so_version = $real_php_version ? {
+    '55' => '1.0.0',
+    '56' => '1.0.0',
+    default => '1.0.1'
+  }
+
+  $arch = $::architecture ? {
+    'x86_64' => '64bit',
+    'amd64' => '64bit',
+    /i\d86/ => '32bit',
+    default => false
+  }
+
+  $oldphp = defined(Class['oldphp'])
+
+  if $oldphp and !defined(Class['php::params']) {
+    include ::php::params
+  }
+
+  package {
+    'php5-memcached':
+      ensure => absent
+  }
+
+  file {
+    '/tmp/AwsElasticCacheClusterClient.tgz':
+      ensure  => file,
+      source  => "puppet:///modules/memcached/tmp/AmazonElastiCacheClusterClient-${$version}-PHP${$real_php_version}-${$arch}.tgz",
+      mode    => '0644',
+      owner   => root,
+      group   => root;
+  }
+
+  if $oldphp {
+    $so_name = "amazon-elasticache-cluster-client-${$version}-${$real_php_version}.so"
+    exec { 'pecl_install_memcached':
+      command => "/bin/tar -xvf /tmp/AwsElasticCacheClusterClient.tgz -C /tmp/ AmazonElastiCacheClusterClient-${$so_version}/amazon-elasticache-cluster-client.so && /bin/mv /tmp/AmazonElastiCacheClusterClient-${$so_version}/amazon-elasticache-cluster-client.so /usr/lib/php5/20121212/${$so_name}",
+      require => File['/tmp/AwsElasticCacheClusterClient.tgz'],
+      creates => "/usr/lib/php5/20121212/${$so_name}"
+    }
+  } elsif $real_php_version == '70' {
+    $so_name = "amazon-elasticache-cluster-client-${$version}-${$real_php_version}.so"
+    # PHP 7.0 has a custom install "method".
+    # See: http://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/Appendix.PHPAutoDiscoverySetup.html#Appendix.PHPAutoDiscoverySetup.Installing.PHP7x
+    exec { 'pecl_install_memcached':
+      command => "/bin/tar -xvf /tmp/AwsElasticCacheClusterClient.tgz -C /tmp/ artifact/amazon-elasticache-cluster-client.so && /bin/mv /tmp/artifact/amazon-elasticache-cluster-client.so /usr/lib/php/20151012/${$so_name}",
+      require => File['/tmp/AwsElasticCacheClusterClient.tgz'],
+      creates => "/usr/lib/php/20151012/${$so_name}"
+    }
+  } else {
+    $so_name = "amazon-elasticache-cluster-client.so"
+    exec { 'pecl_install_memcached':
+      command => '/usr/bin/pecl upgrade /tmp/AwsElasticCacheClusterClient.tgz',
+      require => [
+        Class['::php::pear'],
+        Class['::php::dev'],
+        File['/tmp/AwsElasticCacheClusterClient.tgz']
+      ]
+    }
+  }
+
+  if $oldphp {
+    $config_root_ini = '/etc/php5/mods-available'
+  } else {
+    $config_root_ini = pick_default($::php::config_root_ini, $::php::params::config_root_ini)
+  }
+
+  ::php::config { 'memcached':
+    file    => "${config_root_ini}/memcached.ini",
+    config  => {
+      'extension' => $so_name
+    },
+    require => Exec['pecl_install_memcached'],
+  }
+
+  if $oldphp {
+    if !defined(Package['php5-common']) {
+      package { 'php5-common':
+        ensure => installed
+      }
+    }
+
+    $ext_tool_enable  = '/usr/sbin/php5enmod'
+    $ext_tool_query   = '/usr/sbin/php5query'
+    $require = [
+      ::Php::Config['memcached'],
+      Package['php5-common'],
+    ]
+  } else {
+    $ext_tool_enable  = pick_default($::php::ext_tool_enable, $::php::params::ext_tool_enable)
+    $ext_tool_query   = pick_default($::php::ext_tool_query, $::php::params::ext_tool_query)
+    $require          = ::Php::Config['memcached']
+  }
+
+  # Ubuntu/Debian systems use the mods-available folder. We need to enable
+  # settings files ourselves with php5enmod command.
+  exec { "${ext_tool_enable} -s ALL memcached":
+    onlyif  => "${ext_tool_query} -s cli -m memcached | /bin/grep 'No module matches memcached'",
+    require => $require,
+  }
+
+  if defined(Service['php-fpm']) {
+    Exec["${ext_tool_enable} -s ALL memcached"] ~> Service['php-fpm']
+  }
+
+  if defined(Service['php5-fpm']) {
+    Exec["${ext_tool_enable} -s ALL memcached"] ~> Service['php5-fpm']
+  }
+
+}
